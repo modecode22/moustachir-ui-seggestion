22f0b31366e16d3e394ce8bf6dc54dbfe9ab857b
modecode22 <epiccoder00@gmail.com>
2025-03-27 14:05:30 +0100

    ✨ feat : handled the styles of the header when in the consultent page

--- apps/web/src/components/layout/Header/HeaderWithStyles.tsx
+++ apps/web/src/components/layout/Header/HeaderWithStyles.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 const HeaderWithStyles = () => {
+  const path = usePathname();
   useEffect(() => {
     const header = document.getElementById("main-header");
 
@@ -9,15 +11,28 @@ const HeaderWithStyles = () => {
 
     const handleScroll = () => {
       const offset = window.scrollY;
-      if (offset > 60) {
-        header.classList.add("shadow-sm", "bg-neutral-50");
-        header.classList.remove("bg-transparent");
+      if (!path.startsWith("/consultants")) {
+        header.classList.add("text-primary-500");
+        header.classList.remove("text-neutral-50");
+        if (offset > 60) {
+          header.classList.add("shadow-sm", "bg-neutral-50");
+          header.classList.remove("bg-transparent");
+        } else {
+          header.classList.remove("shadow-sm", "bg-neutral-50");
+          header.classList.add("bg-transparent");
+        }
       } else {
-        header.classList.remove("shadow-sm", "bg-neutral-50");
-        header.classList.add("bg-transparent");
+        header.classList.add("text-neutral-50");
+        header.classList.remove("text-primary-500");
+        if (offset > 60) {
+          header.classList.add("shadow-sm", "bg-neutral-50","text-primary-500");
+          header.classList.remove("bg-transparent");
+        } else {
+          header.classList.remove("shadow-sm", "bg-neutral-50", "text-primary-500");
+          header.classList.add("bg-transparent");
+        }
       }
     };
-
     // Initial check
     handleScroll();
 
@@ -28,7 +43,7 @@ const HeaderWithStyles = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [path]);
 
   // This component doesn't render anything
   return null;
